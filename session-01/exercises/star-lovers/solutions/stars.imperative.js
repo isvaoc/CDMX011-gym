7bfa7a57b127123849d3a5b931c182051dcc2298
Isabel <isvaoc@gmail.com>
2021-07-29 10:08:35 -0500

    solucion del primer problema

--- session-01/exercises/star-lovers/solutions/stars.imperative.js
+++ session-01/exercises/star-lovers/solutions/stars.imperative.js
@@ -1,3 +1,25 @@
-export const printStar = (numero) => {};
+export const printStar = (numero) => {
+    let string="";
+    if (numero == 0){
+        string="";
+    } else{
+        for (let i=1; i<=numero; i++){
+            string= string +"*"
+        }
+    }
+    return string
+};
 
-export const printArrayStar = (numero) => {};
+export const printArrayStar = (numero) => {
+    let stringList=[]
+    if (numero == 0){
+        stringList=[]
+    } else{
+        let string=""
+        for (let i=1; i<=numero; i++){
+            string= string +"*"
+            stringList.push(string)
+        }
+    }
+    return stringList
+};
